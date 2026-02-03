15c04a167cbc7e487b0d850d126e10fedb3cf7ee
bettymugan <demiandreu@gmail.com>
2026-02-03 13:37:36 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Landing from './pages/Landing'
 import FormularioNRUA from './pages/FormularioNRUA'
+import Exito from './pages/Exito'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/formulario" element={<FormularioNRUA />} />
         <Route path="/formulario-nrua" element={<FormularioNRUA />} />
+        <Route path="/exito" element={<Exito />} />
       </Routes>
     </BrowserRouter>
   )
