4bdde419f0d0a1e3fb72ebcfd1354ed1986cca7b
bettymugan <demiandreu@gmail.com>
2026-02-03 08:30:59 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Landing from './pages/Landing'
+import FormularioNRUA from './pages/FormularioNRUA'
 
 function App() {
-  return <Landing />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/formulario" element={<FormularioNRUA />} />
+        <Route path="/formulario-nrua" element={<FormularioNRUA />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
